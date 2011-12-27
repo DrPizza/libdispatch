11750605fd6a5d6f456fd86cf4c3a68c0408e75d
Peter Bright <drpizza@quiscalusmexicanus.org>
2011-12-27 00:25:38 +0000

    Might as well implement gettimeofday() properly.

--- libdispatch/platform/windows/sys/time.c
+++ libdispatch/platform/windows/sys/time.c
@@ -4,6 +4,8 @@
 #include <SDKDDKVer.h>
 #include <Windows.h>
 
+#include <time.h>
+
 struct timeval {
 	long tv_sec;
 	long tv_usec;
@@ -15,20 +17,39 @@ static const unsigned __int64 intervals_per_microsecond = 10ULL;
 static const unsigned __int64 intervals_since_epoch     = 116444736000000000ULL;
 static const unsigned __int64 microseconds_since_epoch  = 11644473600000000ULL;
 
-int gettimeofday(struct timeval* tp, void* tzp)
+int gettimeofday(struct timeval* tv, struct timezone* tz)
 {
-	FILETIME file_time;
-	ULARGE_INTEGER ularge;
+	static int tzflag = 0;
 
-	UNREFERENCED_PARAMETER(tzp);
+	if(NULL != tv)
+	{
+		FILETIME file_time;
+		ULARGE_INTEGER ularge;
 
-	GetSystemTimeAsFileTime(&file_time);
-	ularge.LowPart = file_time.dwLowDateTime;
-	ularge.HighPart = file_time.dwHighDateTime;
-	ularge.QuadPart -= intervals_since_epoch;
-	ularge.QuadPart /= intervals_per_microsecond;
-	tp->tv_sec = (long) (ularge.QuadPart / microseconds_per_second);
-	tp->tv_usec = (long) (ularge.QuadPart % microseconds_per_second);
+		GetSystemTimeAsFileTime(&file_time);
+		ularge.LowPart = file_time.dwLowDateTime;
+		ularge.HighPart = file_time.dwHighDateTime;
+		ularge.QuadPart -= intervals_since_epoch;
+		ularge.QuadPart /= intervals_per_microsecond;
+		tv->tv_sec = (long) (ularge.QuadPart / microseconds_per_second);
+		tv->tv_usec = (long) (ularge.QuadPart % microseconds_per_second);
+	}
+
+	if (NULL != tz)
+	{
+		long seconds = 0;
+		int hours = 0;
+		if (!tzflag)
+		{
+			_tzset();
+			tzflag++;
+		}
+		_get_timezone(&seconds);
+		_get_daylight(&hours);
+
+		tz->tz_minuteswest = seconds / 60;
+		tz->tz_dsttime = hours;
+	}
 
 	return 0;
 }
--- libdispatch/platform/windows/sys/time.h
+++ libdispatch/platform/windows/sys/time.h
@@ -6,8 +6,13 @@ extern "C" {
 #endif
 
 struct timeval;
+struct timezone 
+{
+	int  tz_minuteswest; /* minutes W of Greenwich */
+	int  tz_dsttime;     /* type of dst correction */
+};
 
-int gettimeofday(struct timeval* tp, void* tzp);
+int gettimeofday(struct timeval* tp, struct timezone* tzp);
 
 #ifdef __cplusplus
 }
